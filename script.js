782c6588aa06c05a4a447891b2136d7e327f4a46
davizinleozin <145779491+davizinleozin@users.noreply.github.com>
2025-09-04 11:44:48 -0300

    ,,,

--- script.js
+++ script.js
@@ -14,8 +14,21 @@ enunciado:
     "2. Você deixaria uma IA tomar decisões médicas críticas sobre a sua saúde?"
 alternativa: ["sim!" ,"não!"]
 },
-
-
+{
+enunciado:
+"1. Acredita que governos vão usar a IA mais para controle social do que para o bem comum?"
+alternativa:["sim!" , "não!"]
+},
+{
+    enunciado:
+    "4. Você confiaria em uma IA para educar seus filhos no lugar de um professor humano?"
+alternativa:["sim!" , "não!"]
+}
+{
+enunciado:
+"5.Você acha que a IA pode desenvolver uma consciência própria?"
+alternativa:["sim!" , "não!"]
+}
 
 
 
