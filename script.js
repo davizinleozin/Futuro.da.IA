b50b9af2e1cd73d89f20d52f33f3c7e446a8433b
davizinleozin <145779491+davizinleozin@users.noreply.github.com>
2025-09-04 11:30:03 -0300

    ......

--- script.js
+++ script.js
@@ -3,4 +3,23 @@ const caixaPergunta = document.querySelector(".caixa-pergunta");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");]0-=
-const pergunta = 
+const pergunta = [
+{
+enunciado: 
+"1. Você acredita que um dia será possível se apaixonar por uma IA como por uma pessoa real.
+   alternativa["sim!" , "não!"] 
+},
+{
+    enunciado: 
+    ""2. Você deixaria uma IA tomar decisões médicas críticas sobre a sua saúde?"
+alternativa: []
+
+
+}
+
+
+
+
+
+
+]
